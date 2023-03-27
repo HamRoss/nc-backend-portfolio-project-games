ec7e28e5382b9eead574a13c17d6f3426e5b76b6
HamRoss <rh@rosshamilton.co>
2023-03-27 15:13:10 +0100

    adds get specific review endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -54,3 +54,44 @@ describe("invalid path errors", () => {
       });
   });
 });
+
+describe("api/reviews/:review_id", () => {
+  test("200 GET responds with a single review object with all required properties", () => {
+    return request(app)
+      .get("/api/reviews/1")
+      .expect(200)
+      .then(({ body }) => {
+        const { review } = body;
+        expect(review).toEqual({
+          review_id: 1,
+          title: "Agricola",
+          designer: "Uwe Rosenberg",
+          owner: "mallionaire",
+          review_img_url:
+            "https://images.pexels.com/photos/974314/pexels-photo-974314.jpeg?w=700&h=700",
+          review_body: "Farmyard fun!",
+          category: "euro game",
+          created_at: "2021-01-18T10:00:20.514Z",
+          votes: 1,
+        });
+      });
+  });
+  test("404 GET returns not found message when review id is valid but does not exist", () => {
+    return request(app)
+      .get("/api/reviews/999")
+      .expect(404)
+      .then(({ body }) => {
+        const { msg } = body;
+        expect(msg).toBe("No review found for review_id 999");
+      });
+  });
+  test("400 GET returns bad request message when review id is not an integer", () => {
+    return request(app)
+      .get("/api/reviews/sausage")
+      .expect(400)
+      .then(({ body }) => {
+        const { msg } = body;
+        expect(msg).toBe("Bad request");
+      });
+  });
+});
--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const { getCategories } = require("./controllers/category-controller");
+const { getReview } = require("./controllers/review-controller");
 
 app.get("/api/categories", getCategories);
+
+app.get("/api/reviews/:review_id", getReview);
 
 app.use("*", (req, res, next) => {
   next({ status: 404, msg: "Data not found" });
@@ -12,8 +15,14 @@ app.use((err, req, res, next) => {
   if (err.status && err.msg) {
     res.status(err.status).send(err);
   } else {
-    res.status(500).send("Internal server error");
+    next(err);
   }
+});
+
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad request" });
+  } else res.status(500).send({ msg: "Internal Server Error" });
 });
 
 module.exports = app;
